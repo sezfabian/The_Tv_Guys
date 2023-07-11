59da53b145c24e49f97c1ddb795692be9673d780
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-07-11 06:16:34 +0300

    sessions

--- setup2.sh
+++ setup2.sh
@@ -5,4 +5,21 @@ bash install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+npm install -g npm@9.8.0
+npm install -g @vue/cli
+
+
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+sudo apt -y upgrade
+sudo apt -y install python3
+sudo apt -y install python3-pip
+sudo pip install flask
+sudo pip install -U flask-cors
+sudo pip install pymysql
+sudo pip install flask-bcrypt
+pip install sqlalchemy
 
